088eaf130a5e6ac313857e182af86480caa1d2d7
Sara Millar <snmillar@ucsd.edu>
2014-04-17 14:42:32 -0700

    actually reverting this time

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.get('/auth/facebook/callback', passport.authenticate('facebook-canvas', { fa
 	req.session.fbconfig = fbconfig;
 	res.redirect('/');
 });
-app.post('/auth/facebook/canvas', passport.authenticate('facebook-canvas', { successRedirect: '/', failureRedirect: '/auth/facebook/canvas/autologin'}), function(req,res){
+app.post('/auth/facebook/canvas', passport.authenticate('facebook-canvas', { failureRedirect: '/auth/facebook/canvas/autologin'}), function(req,res){
 	req.session.facebook = true;
 	req.session.fbconfig = fbconfig;
 	res.redirect('/');
