2408c7efb90abb0ed51467ce7279381c0cb9201f
Sara Millar <snmillar@ucsd.edu>
2014-04-18 13:53:56 -0700

    debugging

--- routes/profile.js
+++ routes/profile.js
@@ -6,11 +6,13 @@ exports.view = function(req, res) {
 	data.twitter_auth = req.session.twitter;
 	if(typeof req.session.fbconfig != 'undefined'){
 		graph.setAccessToken(req.session.fbconfig.access_token);
-		graph.get("me", function(err, res){
-			console.log(res);
+		graph.get("me", function(err, gres){
+			console.log(gres);
 		});
 
 		var qres = graph.get("me?fields=name");
+	}else{
+		console.log('req.session.fbconfig is undefined');
 	}
 
 	res.render('profile', data);
