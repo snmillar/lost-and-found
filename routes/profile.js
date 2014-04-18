9a45aaf06536742a3341398dbfb28b75500aed8c
Sara Millar <snmillar@ucsd.edu>
2014-04-18 13:57:02 -0700

    added debug statements

--- routes/profile.js
+++ routes/profile.js
@@ -5,11 +5,14 @@ exports.view = function(req, res) {
 	data.facebook_auth = req.session.facebook;
 	data.twitter_auth = req.session.twitter;
 	if(typeof req.session.fbconfig != 'undefined'){
+		console.log('setting the access token');
 		graph.setAccessToken(req.session.fbconfig.access_token);
+		console.log('getting the info');
 		graph.get("me", function(err, gres){
 			console.log(gres);
 		});
 
+		console.log('getting specific info');
 		var qres = graph.get("me?fields=name");
 	}else{
 		console.log('req.session.fbconfig is undefined');
