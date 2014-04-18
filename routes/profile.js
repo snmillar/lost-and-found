4c6457be708d4da704dbcd5f99662b48a2c9235c
Sara Millar <snmillar@ucsd.edu>
2014-04-18 13:43:32 -0700

    trying to access facebook data

--- routes/profile.js
+++ routes/profile.js
@@ -1,3 +1,17 @@
+var graph = require('fbgraph');
+
 exports.view = function(req, res) {
-	res.render('profile', {facebook_auth: req.session.facebook, twitter_auth: req.session.twitter});
+	var data = { };
+	data.facebook_auth = req.session.facebook;
+	data.twitter_auth = req.session.twitter;
+	if(typeof req.session.fbconfig != 'undefined'){
+		graph.setAccessToken(req.session.fbconfig.access_token);
+		graph.get("me", function(err, res){
+			console.log(res);
+		});
+
+		var qres = graph.get("me?fields=name");
+	}
+
+	res.render('profile', data);
 }
